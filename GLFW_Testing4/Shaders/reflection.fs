480273e4cd71d430d064d2ebbce03fc99778a38c
Jack <y23657689@gmail.com>
2020-11-20 17:34:47 +0800

    Add Some Note

--- GLFW_Testing4/Shaders/reflection.fs
+++ GLFW_Testing4/Shaders/reflection.fs
@@ -10,10 +10,12 @@ uniform samplerCube skybox;
 uniform sampler2D texture1;
 
 void main() {    
+    // reflect
     //vec3 viewDir = normalize(Position - cameraPos);
     //vec3 reflectDir = reflect(viewDir, normalize(Normal));
     //FragColor = vec4(texture(skybox, reflectDir).rgb, 1.0);
-
+       
+    // refract
     float ratio = 1.00 / 1.33;
     vec3 viewDir = normalize(Position - cameraPos);
     vec3 refractDir = refract(viewDir, normalize(Normal), ratio);
